6576d06e528fb3db306c693e057cce14191baf5b
Xiao <mx63@umdnj.edu>
2020-01-28 12:32:25 -0500

    Added the function and the addin configuration

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,19 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' Parse selected "Windows" path to an "R-usable" one
+#'
+#' @return
+#' @export
+#' @importFrom rstudioapi getActiveDocumentContext
+#' @importFrom magrittr '%>%'
+#'
+#' @examples
+path_parse <- function() {
+  getActiveDocumentContext()[['selection']][[1]][['text']] %>%
+  { gsub('\\\\', '/', .) } %>%
+  { gsub('//', '/', .) } %>%    # To remove double f. slashes
+  { ifelse(check_for_quotes(.), insertText(.), insertText(paste0('\'', ., '\''))) }
 }
+### Old function:
+# path_parse <- function() {
+#   getActiveDocumentContext()[['selection']][[1]][['text']] %>%
+#   { chartr('\\', '/', .) } %>% { insertText(paste0('\'', ., '\'')) }
+# }
